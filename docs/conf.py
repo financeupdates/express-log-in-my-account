bf80a5eebf2fa92eb07a557e421f1765c30529b5
financeupdates <nandininama57@gmail.com>
2026-07-17 13:40:51 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
 
-project = 'Ancestry Login Guide'
-author = 'Ancestry Guide'
+project = 'american express log in my account'
+author = 'american express log in my account'
 release = '1.0'
 
 extensions = []
@@ -18,4 +18,4 @@ html_static_path = ['_static']
 
 language = 'en'
 
-html_title = "Ancestry.com Login Guide 2026"
+html_title = "american express log in my account"
